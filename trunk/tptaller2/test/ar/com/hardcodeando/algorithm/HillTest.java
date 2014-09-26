5a80343fc300b0bb53ce3be2b9cac63580b09219
huallpa.nestor@gmail.com <huallpa.nestor@gmail.com@49307b52-acec-b62e-620b-61b559b46a4c>
2014-09-26 04:34:16 +0000

    Agrego unos casos de test

--- trunk/tptaller2/test/ar/com/hardcodeando/algorithm/HillTest.java
+++ trunk/tptaller2/test/ar/com/hardcodeando/algorithm/HillTest.java
@@ -1,7 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Referencias:
+ * http://en.wikipedia.org/wiki/Minor_(linear_algebra)#Matrix_of_cofactors
+ * http://www.codeproject.com/Articles/405128/Matrix-operations-in-Java
+ * http://www.unc.edu/~marzuola/Math547_S13/Math547_S13_Projects/R_Doyle_Section001_Cryptography.pdf
+ * http://cryptohill.blogspot.com.ar/2011/06/parte-1-cifrado-de-hill-completada.html
+ *
  */
 package ar.com.hardcodeando.algorithm;
 
@@ -36,14 +39,86 @@ public class HillTest {
     
     @After
     public void tearDown() {
+
     }
 
 
     @Test
-    public void encriptarUnaLetra() {
+    public void verificarClaveValida() {
         HillCipher hill = new HillCipher();
+        
+        // Armar matriz M inversible
+        // mcd(det(M),26) = 1 con esto nos asegurarmos de desifrar
+        int[][] clave = {{3,2},{5,7}};
+       
+        assertTrue(hill.validarClave(clave));
+    }
+    @Test
+    public void verificarOtraClaveValida() {
+        HillCipher hill = new HillCipher();
+        
+        // Armar matriz M inversible
+        // mcd(det(M),26) = 1 con esto nos asegurarmos de desifrar
+        int[][] clave = {{4,9},{13,18}};
+        
+        hill.validarClave(clave);
+        
+        assertTrue(hill.validarClave(clave));
+    }
+    
+    @Test
+    public void verificarClaveInvalida() {
+        HillCipher hill = new HillCipher();
+        
+        
+        int[][] clave = {{1,1},{1,1}};
+               
+        assertFalse(hill.validarClave(clave));
+    }
+    
+    
+    @Test
+    public void encriptarHola() {
+        HillCipher hill = new HillCipher();
+        
         int[][] clave = null;
-        String msgEncriptado = hill.encriptar("A", clave);
+        
+        hill.validarClave(clave);
+        
+        String msgEncriptado = hill.encriptar("HOLAS", clave);
         assertEquals("ASD", msgEncriptado);
     }
+    
+    @Test
+    public void desencriptarHola() {
+        HillCipher hill = new HillCipher();
+        
+        int[][] clave = null;
+       
+        String msgEncriptado = hill.desencriptar("A", clave);
+        assertEquals("ASD", msgEncriptado);
+    }
+    
+    @Test
+    public void encriptarTallerII() {
+        HillCipher hill = new HillCipher();
+        
+        int[][] clave = null;
+        
+        hill.validarClave(clave);
+        
+        String msgEncriptado = hill.encriptar("TALLERII", clave);
+        assertEquals("ASD", msgEncriptado);
+    }
+    
+    @Test
+    public void desencriptarTallerII() {
+        HillCipher hill = new HillCipher();
+        
+        int[][] clave = null;
+       
+        String msgEncriptado = hill.desencriptar("A", clave);
+        assertEquals("ASD", msgEncriptado);
+    }
+    
 }
