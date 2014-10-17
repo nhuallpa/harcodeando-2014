b493579082758984aeab74f707070c83f53e9c6d
estefi1984@gmail.com <estefi1984@gmail.com@49307b52-acec-b62e-620b-61b559b46a4c>
2014-10-17 04:35:02 +0000

    Agrego rondas al paso 4 del md5

--- trunk/tptaller2/src/ar/com/hardcodeando/algorithm/MD5.java
+++ trunk/tptaller2/src/ar/com/hardcodeando/algorithm/MD5.java
@@ -67,7 +67,7 @@ class MD5 {
 		long x[] = new long[16];
 	
 		x = decode(bytBlock);
-
+                System.out.println("" + " A: " + lngA + "B: " + lngB + "C: " + lngC+ " D: " +lngD );
 		/* Ronda 1 */
 		System.out.println("Ronda 1");
 		lngA = ff(lngA, lngB, lngC, lngD, x[0], S11, 0xd76aa478L); /* 1 */
@@ -75,28 +75,28 @@ class MD5 {
 		lngC = ff(lngC, lngD, lngA, lngB, x[2], S13, 0x242070dbL); /* 3 */
 		lngB = ff(lngB, lngC, lngD, lngA, x[3], S14, 0xc1bdceeeL); /* 4 */
 		if (pasos == 16) {
-			System.out.println("FF A " + lngA + "FF D " + lngD + "FF C " + lngC + "FF B " + lngB);
+			System.out.println("FF A " + Long.toHexString(lngA) + " D " + Long.toHexString(lngD) + " C " + Long.toHexString(lngC) + " B " + Long.toHexString(lngB));
 		}
 		lngA = ff(lngA, lngB, lngC, lngD, x[4], S11, 0xf57c0fafL); /* 5 */
 		lngD = ff(lngD, lngA, lngB, lngC, x[5], S12, 0x4787c62aL); /* 6 */
 		lngC = ff(lngC, lngD, lngA, lngB, x[6], S13, 0xa8304613L); /* 7 */
 		lngB = ff(lngB, lngC, lngD, lngA, x[7], S14, 0xfd469501L); /* 8 */
 		if (pasos == 16) {
-			System.out.println("FF A " + lngA + "FF D " + lngD + "FF C " + lngC + "FF B " + lngB);
+			System.out.println("FF A " + Long.toHexString(lngA) + " D " + Long.toHexString(lngD) + " C " + Long.toHexString(lngC) + " B " + Long.toHexString(lngB));
 		}
 		lngA = ff(lngA, lngB, lngC, lngD, x[8], S11, 0x698098d8L); /* 9 */
 		lngD = ff(lngD, lngA, lngB, lngC, x[9], S12, 0x8b44f7afL); /* 10 */
 		lngC = ff(lngC, lngD, lngA, lngB, x[10], S13, 0xffff5bb1L); /* 11 */
 		lngB = ff(lngB, lngC, lngD, lngA, x[11], S14, 0x895cd7beL); /* 12 */
 		if (pasos == 16) {
-			System.out.println("FF A " + lngA + "FF D " + lngD + "FF C " + lngC + "FF B " + lngB);
+			System.out.println("FF A " + Long.toHexString(lngA) + " D " + Long.toHexString(lngD) + " C " + Long.toHexString(lngC) + " B " + Long.toHexString(lngB));
 		}
 		lngA = ff(lngA, lngB, lngC, lngD, x[12], S11, 0x6b901122L); /* 13 */
 		lngD = ff(lngD, lngA, lngB, lngC, x[13], S12, 0xfd987193L); /* 14 */
 		lngC = ff(lngC, lngD, lngA, lngB, x[14], S13, 0xa679438eL); /* 15 */
 		lngB = ff(lngB, lngC, lngD, lngA, x[15], S14, 0x49b40821L); /* 16 */
 		if (pasos == 4 || pasos == 16) {
-			System.out.println("FF A " + lngA + "FF D " + lngD + "FF C " + lngC + "FF B " + lngB);
+			System.out.println("FF A " + Long.toHexString(lngA) + " D " + Long.toHexString(lngD) + " C " + Long.toHexString(lngC) + " B " + Long.toHexString(lngB));
 		}
 		
 		/* Ronda 2 */
@@ -106,28 +106,28 @@ class MD5 {
 		lngC = gg(lngC, lngD, lngA, lngB, x[11], S23, 0x265e5a51L); /* 19 */
 		lngB = gg(lngB, lngC, lngD, lngA, x[0], S24, 0xe9b6c7aaL); /* 20 */
 		if (pasos == 16) {
-			System.out.println("GG A " + lngA + "GG D " + lngD + "GG C " + lngC + "GG B " + lngB);
+			System.out.println("GG A " + Long.toHexString(lngA) + " D " + Long.toHexString(lngD) + " C " + Long.toHexString(lngC) + " B " + Long.toHexString(lngB));
 		}
 		lngA = gg(lngA, lngB, lngC, lngD, x[5], S21, 0xd62f105dL); /* 21 */
 		lngD = gg(lngD, lngA, lngB, lngC, x[10], S22, 0x2441453L); /* 22 */
 		lngC = gg(lngC, lngD, lngA, lngB, x[15], S23, 0xd8a1e681L); /* 23 */
 		lngB = gg(lngB, lngC, lngD, lngA, x[4], S24, 0xe7d3fbc8L); /* 24 */
 		if (pasos == 16) {
-			System.out.println("GG A " + lngA + "GG D " + lngD + "GG C " + lngC + "GG B " + lngB);
+			System.out.println("GG A " + Long.toHexString(lngA) + " D " + Long.toHexString(lngD) + " C " + Long.toHexString(lngC) + " B " + Long.toHexString(lngB));
 		}
 		lngA = gg(lngA, lngB, lngC, lngD, x[9], S21, 0x21e1cde6L); /* 25 */
 		lngD = gg(lngD, lngA, lngB, lngC, x[14], S22, 0xc33707d6L); /* 26 */
 		lngC = gg(lngC, lngD, lngA, lngB, x[3], S23, 0xf4d50d87L); /* 27 */
 		lngB = gg(lngB, lngC, lngD, lngA, x[8], S24, 0x455a14edL); /* 28 */
 		if (pasos == 16) {
-			System.out.println("GG A " + lngA + "GG D " + lngD + "GG C " + lngC + "GG B " + lngB);
+			System.out.println("GG A " + Long.toHexString(lngA) + " D " + Long.toHexString(lngD) + " C " + Long.toHexString(lngC) + " B " + Long.toHexString(lngB));
 		}
 		lngA = gg(lngA, lngB, lngC, lngD, x[13], S21, 0xa9e3e905L); /* 29 */
 		lngD = gg(lngD, lngA, lngB, lngC, x[2], S22, 0xfcefa3f8L); /* 30 */
 		lngC = gg(lngC, lngD, lngA, lngB, x[7], S23, 0x676f02d9L); /* 31 */
 		lngB = gg(lngB, lngC, lngD, lngA, x[12], S24, 0x8d2a4c8aL); /* 32 */
 		if (pasos == 4 || pasos == 16) {
-			System.out.println("GG A " + lngA + "GG D " + lngD + "GG C " + lngC + "GG B " + lngB);
+			System.out.println("GG A " + Long.toHexString(lngA) + " D " + Long.toHexString(lngD) + " C " + Long.toHexString(lngC) + " B " + Long.toHexString(lngB));
 		}
 		/* Ronda 3 */
 		System.out.println("Ronda 3 ");
@@ -136,28 +136,28 @@ class MD5 {
 		lngC = hh(lngC, lngD, lngA, lngB, x[11], S33, 0x6d9d6122L); /* 35 */
 		lngB = hh(lngB, lngC, lngD, lngA, x[14], S34, 0xfde5380cL); /* 36 */
 		if (pasos == 16) {
-			System.out.println("HH A " + lngA + "HH D " + lngD + "HH C " + lngC + "HH B " + lngB);
+			System.out.println("HH A " + Long.toHexString(lngA) + " D " + Long.toHexString(lngD) + " C " + Long.toHexString(lngC) + " B " + Long.toHexString(lngB));
 		}
 		lngA = hh(lngA, lngB, lngC, lngD, x[1], S31, 0xa4beea44L); /* 37 */
 		lngD = hh(lngD, lngA, lngB, lngC, x[4], S32, 0x4bdecfa9L); /* 38 */
 		lngC = hh(lngC, lngD, lngA, lngB, x[7], S33, 0xf6bb4b60L); /* 39 */
 		lngB = hh(lngB, lngC, lngD, lngA, x[10], S34, 0xbebfbc70L); /* 40 */
 		if (pasos == 16) {
-			System.out.println("HH A " + lngA + "HH D " + lngD + "HH C " + lngC + "HH B " + lngB);
+			System.out.println("HH A " + Long.toHexString(lngA) + " D " + Long.toHexString(lngD) + " C " + Long.toHexString(lngC) + " B " + Long.toHexString(lngB));
 		}
 		lngA = hh(lngA, lngB, lngC, lngD, x[13], S31, 0x289b7ec6L); /* 41 */
 		lngD = hh(lngD, lngA, lngB, lngC, x[0], S32, 0xeaa127faL); /* 42 */
 		lngC = hh(lngC, lngD, lngA, lngB, x[3], S33, 0xd4ef3085L); /* 43 */
 		lngB = hh(lngB, lngC, lngD, lngA, x[6], S34, 0x4881d05L); /* 44 */
 		if (pasos == 16) {
-			System.out.println("HH A " + lngA + "HH D " + lngD + "HH C " + lngC + "HH B " + lngB);
+			System.out.println("HH A " + Long.toHexString(lngA) + " D " + Long.toHexString(lngD) + " C " + Long.toHexString(lngC) + " B " + Long.toHexString(lngB));
 		}
 		lngA = hh(lngA, lngB, lngC, lngD, x[9], S31, 0xd9d4d039L); /* 45 */
 		lngD = hh(lngD, lngA, lngB, lngC, x[12], S32, 0xe6db99e5L); /* 46 */
 		lngC = hh(lngC, lngD, lngA, lngB, x[15], S33, 0x1fa27cf8L); /* 47 */
 		lngB = hh(lngB, lngC, lngD, lngA, x[2], S34, 0xc4ac5665L); /* 48 */
 		if (pasos == 4 || pasos == 16) {
-			System.out.println("HH A " + lngA + "HH D " + lngD + "HH C " + lngC + "HH B " + lngB);
+			System.out.println("HH A " + Long.toHexString(lngA) + " D " + Long.toHexString(lngD) + " C " + Long.toHexString(lngC) + " B " + Long.toHexString(lngB));
 		}
 		/* Ronda 4 */
 		System.out.println("Ronda 4 ");
@@ -166,40 +166,45 @@ class MD5 {
 		lngC = ii(lngC, lngD, lngA, lngB, x[14], S43, 0xab9423a7L); /* 51 */
 		lngB = ii(lngB, lngC, lngD, lngA, x[5], S44, 0xfc93a039L); /* 52 */
 		if (pasos == 16) {
-			System.out.println("II A " + lngA + "II D " + lngD + "II C " + lngC + "II B " + lngB);
+			System.out.println("II A " + Long.toHexString(lngA) + " D " + Long.toHexString(lngD) + " C " + Long.toHexString(lngC) + " B " + Long.toHexString(lngB));
 		}
 		lngA = ii(lngA, lngB, lngC, lngD, x[12], S41, 0x655b59c3L); /* 53 */
 		lngD = ii(lngD, lngA, lngB, lngC, x[3], S42, 0x8f0ccc92L); /* 54 */
 		lngC = ii(lngC, lngD, lngA, lngB, x[10], S43, 0xffeff47dL); /* 55 */
 		lngB = ii(lngB, lngC, lngD, lngA, x[1], S44, 0x85845dd1L); /* 56 */
 		if (pasos == 16) {
-			System.out.println("II A " + lngA + "II D " + lngD + "II C " + lngC + "II B " + lngB);
+			System.out.println("II A " + Long.toHexString(lngA) + " D " + Long.toHexString(lngD) + " C " + Long.toHexString(lngC) + " B " + Long.toHexString(lngB));
 		}
 		lngA = ii(lngA, lngB, lngC, lngD, x[8], S41, 0x6fa87e4fL); /* 57 */
 		lngD = ii(lngD, lngA, lngB, lngC, x[15], S42, 0xfe2ce6e0L); /* 58 */
 		lngC = ii(lngC, lngD, lngA, lngB, x[6], S43, 0xa3014314L); /* 59 */
 		lngB = ii(lngB, lngC, lngD, lngA, x[13], S44, 0x4e0811a1L); /* 60 */
 		if (pasos == 16) {
-			System.out.println("II A " + lngA + "II D " + lngD + "II C " + lngC + "II B " + lngB);
+			System.out.println("II A " + Long.toHexString(lngA) + " D " + Long.toHexString(lngD) + " C " + Long.toHexString(lngC) + " B " + Long.toHexString(lngB));
 		}
 		lngA = ii(lngA, lngB, lngC, lngD, x[4], S41, 0xf7537e82L); /* 61 */
 		lngD = ii(lngD, lngA, lngB, lngC, x[11], S42, 0xbd3af235L); /* 62 */
 		lngC = ii(lngC, lngD, lngA, lngB, x[2], S43, 0x2ad7d2bbL); /* 63 */
 		lngB = ii(lngB, lngC, lngD, lngA, x[9], S44, 0xeb86d391L); /* 64 */
 		if (pasos == 1 || pasos == 4 || pasos == 16) {
-			System.out.println("II A " + lngA + "II D " + lngD + "II C " + lngC + "II B " + lngB);
+			System.out.println("II A " + Long.toHexString(lngA) + " D " + Long.toHexString(lngD) + " C " + Long.toHexString(lngC) + " B " + Long.toHexString(lngB));
 		}
         /* Este es el incremento de cada uno de los cuatro registros por el valor que tenian antes de que
 	    este bloque fuera inicializado.	 */
+                System.out.println("A  " + Long.toHexString(lngState[0]));
+                System.out.println("B  " + Long.toHexString(lngState[1]));
+                System.out.println("C  " + Long.toHexString(lngState[2]));
+                System.out.println("D  " + Long.toHexString(lngState[3]));
+                
 		lngState[0] = (lngState[0] + lngA) & 0xFFFFFFFFL; //A = A + AA
 		lngState[1] = (lngState[1] + lngB) & 0xFFFFFFFFL; //B = B + BB
 		lngState[2] = (lngState[2] + lngC) & 0xFFFFFFFFL; //C = C + CC
 		lngState[3] = (lngState[3] + lngD) & 0xFFFFFFFFL;  //D = D + DD
 		System.out.println("Suma final ");
-		System.out.println("A = A + AA " + lngState[0]);
-		System.out.println("B = B + BB " + lngState[1]);
-		System.out.println("C = C + CC " + lngState[2]);
-		System.out.println("D = D + DD " + lngState[3]);
+		System.out.println("A = A + AA: " + Long.toHexString(lngState[0]));
+		System.out.println("B = B + BB: " + Long.toHexString(lngState[1]));
+		System.out.println("C = C + CC: " + Long.toHexString(lngState[2]));
+		System.out.println("D = D + DD: " + Long.toHexString(lngState[3]));
 		
 		x = decode(pad);
 	}
